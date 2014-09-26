7c19ad4e025c873226e6a9e51cff874945db4e1d
Vivek <vivek@Viveks-MacBook-Pro-2.local>
2014-09-26 08:52:43 +0400

    Initial Commit

---
+++ RND_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RND_Swift
+//
+//  Created by Vivek on 9/26/14.
+//  Copyright (c) 2014 Emirates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
